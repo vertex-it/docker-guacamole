1f7e0fc48656a9f7ae6be46a1528bb8403e146fa
milivojsa <milivojsa96@gmail.com>
2021-06-09 14:20:04 +0200

    Bug fix

--- root/etc/cont-init.d/40-postgres.sh
+++ root/etc/cont-init.d/40-postgres.sh
@@ -10,10 +10,14 @@ if [ -e /config/postgres/postgresql.conf ]; then
   echo "Database already configured"
 else
   s6-setuidgid postgres initdb
+  
+  # Adjust PostgreSQL configuration so that remote connections to the database are possible.
+  echo "host all  all    0.0.0.0/0  md5" >> /config/postgres/pg_hba.conf
+
+  # And add ``listen_addresses`` to ``/etc/postgresql/9.3/main/postgresql.conf``
+  echo "listen_addresses='*'" >> /config/postgres/postgresql.conf
+
+  # Restart postgresql service to apply changes
+  /etc/init.d/postgresql restart
 fi
 
-# Adjust PostgreSQL configuration so that remote connections to the database are possible.
-echo "host all  all    0.0.0.0/0  md5" >> /config/postgres/pg_hba.conf
-
-# And add ``listen_addresses`` to ``/etc/postgresql/9.3/main/postgresql.conf``
-echo "listen_addresses='*'" >> /config/postgres/postgresql.conf
