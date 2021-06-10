a38bc8c973e4f5eeccef382e47bcb9e9a40bb56c
milivojsa <milivojsa96@gmail.com>
2021-06-10 15:26:33 +0200

    Temporary comment

--- root/etc/cont-init.d/40-postgres.sh
+++ root/etc/cont-init.d/40-postgres.sh
@@ -11,10 +11,10 @@ if [ -e /config/postgres/postgresql.conf ]; then
 else
   s6-setuidgid postgres initdb
   
-  # Adjust PostgreSQL configuration so that remote connections to the database are possible.
-  echo "host all  all    0.0.0.0/0  trust" >> /config/postgres/pg_hba.conf
+#   # Adjust PostgreSQL configuration so that remote connections to the database are possible.
+#   echo "host all  all    0.0.0.0/0  trust" >> /config/postgres/pg_hba.conf
 
-  # And add ``listen_addresses`` to ``/etc/postgresql/9.3/main/postgresql.conf``
-  echo "listen_addresses='*'" >> /config/postgres/postgresql.conf
+#   # And add ``listen_addresses`` to ``/etc/postgresql/9.3/main/postgresql.conf``
+#   echo "listen_addresses='*'" >> /config/postgres/postgresql.conf
 fi
 
